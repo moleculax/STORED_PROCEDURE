0035e4d316be6a71653c62f1d81b16fb47d5573d
Emilio Gomez <moleculax@gmail.com>
2023-08-08 12:09:26 -0300

    Update SPF_MAIL_VALIDADOR.sql

--- SPF_VALIDA_MAIL.sql
+++ SPF_VALIDA_MAIL.sql
@@ -1,4 +1,4 @@
-CREATE DEFINER=`sistemaspvyt`@`%` FUNCTION `SPF_VALIDA_MAIL`(pMAIL VARCHAR(254)) RETURNS int(1)
+CREATE DEFINER=`root`@`%` FUNCTION `SPF_MAIL_VALIDADOR`(pMAIL VARCHAR(254)) RETURNS int(1)
     READS SQL DATA
 BEGIN
 	DECLARE vpMAIL VARCHAR(254);
@@ -10,11 +10,9 @@ BEGIN
     SET EXPREMATCH = '[a-zA-Z0-9_.-]+([.][a-zA-Z0-9_]+)*[@][a-zA-Z0-9_-]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{2,4}$';
 	SET vRETORNO=0;	
 	EL_LOOP: LOOP
-		-- Esta Funcion Tiene Por Objetivo VALIDAR la Cuenta de MAIL Recibida en (pMAIL)
-		-- 
-        --
-		-- RETORNA 	( vRETORNO = 1 ) SI OK
-		--			( vRETORNO = 0 ) SI ES INACEPTABLE
+		-- Esta Funcion Tiene Por Objetivo VALIDAR la Cuenta de MAIL Recibida
+		-- RESPUESTA 	( RETORNA = 1 ) SI OK
+		--		( RETORNA = 0 ) SI NO ES VALIDO EL MAIL
 		-- ======================================================================
         SELECT LTRIM(RTRIM(vpMAIL)) REGEXP LTRIM(RTRIM(EXPREMATCH)) INTO vRETORNO;
 		LEAVE EL_LOOP;
