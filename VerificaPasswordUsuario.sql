d0e0c1bc8c6f905ce2d18b8c2061e6787ed4b6eb
Developer MoleculaX <moleculax@gmail.com>
2025-08-12 20:35:00 -0300

    modi
    set @usuario = "root"
    , @host = "localhost";
    call VerificaPasswordUsuario(@usuario,@host);

---
+++ VerificaPasswordUsuario.sql
@@ -0,0 +1,19 @@
+CREATE DEFINER=`root`@`localhost` PROCEDURE `VerificaPasswordUsuario`(
+    IN nombre_usuario VARCHAR(100),
+    IN host_usuario VARCHAR(100)
+)
+
+
+BEGIN
+    DECLARE estado VARCHAR(20);
+
+    SELECT 
+        CASE
+            WHEN authentication_string IS NULL OR authentication_string = '' THEN 'Sin contraseña'
+            ELSE 'Con contraseña'
+        END INTO estado
+    FROM mysql.user
+    WHERE User = nombre_usuario AND Host = host_usuario;
+
+    SELECT CONCAT('El usuario "', nombre_usuario, '"@"', host_usuario, '" está: ', estado) AS Resultado;
+END
